28600c8afd93f0810f8656c391045d7bf0c3a328
pawleq <57144337+pawleq@users.noreply.github.com>
2020-02-11 21:43:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,32 +13,38 @@ struct Objektas
     int plot;
     void nusk()
     {
-    cout << "Iveskite varda: ";
-    cin >> vardas;
-    cout << "Iveskite lentele ploti : ";
-    cin >> plot;
-    int ilg = vardas.length();
-    if (vardas[ilg-1]=='a')
-    {
-        pasisveikinimas = " Sveika, ";
-    }
-        else pasisveikinimas = " Sveikas, ";
-    pasisveikinimas += vardas+"!";
-///////////////////////////////////////////////////
-    for (int i = 0; i < pasisveikinimas.length() + 3 ; i++) cout <<"*";
-    cout << endl;
-    for (int i = 0; i < plot; i++) {
-        if (i == plot / 2)
-            cout << "*" + pasisveikinimas + " *" << endl;
-        else {
-            cout << "*";
-            for (int j = 0; j < pasisveikinimas.length(); j++) cout <<" ";
-            cout << " *" << endl;
+        cout << "Iveskite varda: ";
+        cin >> vardas;
+        cout << "Iveskite lentele ploti : ";
+        cin >> plot;
+        int ilg = vardas.length();
+        if (vardas[ilg-1]=='s' ||  vardas[ilg-1]=='r')
+        {
+            pasisveikinimas = " Sveikas, ";
         }
+        else
+            pasisveikinimas = " Sveika, ";
+        pasisveikinimas += vardas+"!";
+///////////////////////////////////////////////////
+        for (int i = 0; i < pasisveikinimas.length() + 3 ; i++)
+            cout <<"*";
+        cout << endl;
+        for (int i = 0; i < plot-2; i++)
+        {
+            if (i == (plot-2) / 2)
+                cout << "*" + pasisveikinimas + " *" << endl;
+            else
+            {
+                cout << "*";
+                for (int j = 0; j < pasisveikinimas.length(); j++)
+                    cout <<" ";
+                cout << " *" << endl;
+            }
+        }
+        for (int i = 0; i < pasisveikinimas.length() + 3; i++)
+            cout <<"*";
+        cout << endl;
     }
-    for (int i = 0; i < pasisveikinimas.length() + 3; i++) cout <<"*";
-    cout << endl;
-}
 };
 int main()
 {
